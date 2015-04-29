8e0840900302cebb548aff1caf355f242ca11e79
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-04-29 10:51:29 +0100

    this is the slowest tutorial I've ever seen.. nothing happens

--- app/addPost/addPost.js
+++ app/addPost/addPost.js
@@ -12,6 +12,23 @@ angular.module('myApp.addPost', [
 
   }])
 
-  .controller('AddPostCtrl', ['$scope', function ($scope) {
+  .controller('AddPostCtrl', ['$scope', '$firebase', function ($scope, $firebase) {
+
+    var firebaseObj = new Firebase("https://angular-and-firebase.firebaseio.com/");
+    var fb = $firebase(firebaseObj);
+
+    $scope.AddPost = function () {
+      var title = $scope.article.title;
+      var post = $scope.article.post;
+
+      fb.$push({
+        title: title,
+        post: post
+      }).then(function (ref) {
+        console.log(ref);
+      }, function (error) {
+        console.log(error);
+      })
+    };
 
   }]);
